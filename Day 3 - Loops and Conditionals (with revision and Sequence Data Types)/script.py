01c7b81fb8c13bf47099f3bca3084d3bc573e77e
devidrees <dev.idrees@gmail.com>
2025-11-18 13:11:03 +0530

    first commit

---
+++ Day 3 - Loops and Conditionals (with revision and Sequence Data Types)/script.py
@@ -0,0 +1,5 @@
+print("I want to know your name!")  # prints a message in the console 
+
+name = input("What is your name? ") # takes input from the user
+
+print(f"Hello {name}! Welcome to python!!!")
